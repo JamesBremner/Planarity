2f9025ff2111d287511bdd5bdb52fe9bf3b0a81e
JamesBremner <james@ravenspoint.com>
2023-12-05 14:20:31 -0500

    walkabout

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,83 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
+#include "cGraph.h"
+#include "cxy.h"
 
-class cGUI : public cStarterGUI
+raven::graph::cGraph g;
+std::vector<cxy> vloc;
+std::vector<int> path;
+
+
+void generate()
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+    g.add("1", "3");
+    g.add("2", "3");
+    g.add("3", "6");
+    g.add("4", "6");
+    g.add("5", "6");
+    vloc.resize(g.vertexCount());
+    vloc[g.find("1")] = cxy(0, 10);
+    vloc[g.find("2")] = cxy(10, 10);
+    vloc[g.find("3")] = cxy(5, 7);
+    vloc[g.find("4")] = cxy(0, 0);
+    vloc[g.find("5")] = cxy(10, 0);
+    vloc[g.find("6")] = cxy(5, 3);
+
+}
+
+void walkaround(int start )
+{
+    if( g.adjacentOut(start).size() > 1 )
+        throw std::runtime_error("walkaround must start at a leaf");
+
+    path.push_back(start);
+    int u = start;
+
+    while (true)
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+        auto vout = g.adjacentOut(u);
+        if (vout.size() == 1)
+        {
+            // u is a leaf, so we must backtrack
+            u = vout[0];
+        }
+        else
+        {
+            int prev = path[path.size() - 2];
 
-        show();
-        run();
+            int next = -1;
+
+            // select next edge with minimum clockwise angle from entering edge
+            double amin = DBL_MAX;
+            for (int v : vout)
+            {
+                double a = -cxy::angle(
+                    vloc[u], vloc[prev],
+                    vloc[u], vloc[v]);
+                if (a < amin)
+                {
+                    amin = a;
+                    next = v;
+                }
+            }
+            u = next;
+        }
+        path.push_back(u);
+        if (u == start)
+            return;
     }
-
-private:
-    wex::label &lb;
-};
+}
 
 main()
 {
-    cGUI theGUI;
+
+    generate();
+    walkaround(0);
+
+    for (int vp : path)
+        std::cout << g.userName(vp) << " ";
+    std::cout << "\n";
+
     return 0;
 }
